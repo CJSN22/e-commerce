0e9968255551d2aaeddeaa74b0b94eeccc7800d0
CJSN22 <cjsn22@gmail.com>
2019-11-17 21:36:50 -0500

    updating stripe integration and updating favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_hyoQ2l6JmZ9wBxqCOD4P5uiC00dYNj7e3Q';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='Crwn Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://sendeyo.com/up/d/f3eb2117da'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+
+};
+
+
+export default StripeCheckoutButton;
